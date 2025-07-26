311a9a2951b6d977634e3712fc03405b90091deb
biibekk <bibek.srestha20@gmail.com>
2025-07-26 23:30:45 +0530

    complete OSSL_Assgn_1_Q2.c

--- OSSL_Assgn_1_Q2.c
+++ OSSL_Assgn_1_Q2.c
@@ -1,1 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter size of array: ");
+    scanf("%d",&n);
+    
+    float avg;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("enter element %d :",i);
+        scanf("%d",&arr[i]);
+        avg += arr[i];
+    }
+    
+    avg /= n;
+    
+    printf("the average of the array is : %f",avg);
+
+    return 0;
+}
